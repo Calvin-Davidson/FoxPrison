c22c39d882bd4da0c8870679bca94c02c050b745
Calvin-Davidson <53999981+Calvin-Davidson@users.noreply.github.com>
2026-02-04 18:16:58 +0100

    cleanup + fixed block drops

--- src/main/java/nl/trifox/foxprison/modules/mines/events/MineBlockBreakEvent.java
+++ src/main/java/nl/trifox/foxprison/modules/mines/events/MineBlockBreakEvent.java
@@ -2,27 +2,14 @@ package nl.trifox.foxprison.modules.mines.events;
 
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
-import com.hypixel.hytale.component.Holder;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.EntityEventSystem;
-import com.hypixel.hytale.math.vector.Vector3d;
-import com.hypixel.hytale.math.vector.Vector3f;
-import com.hypixel.hytale.protocol.GameMode;
-import com.hypixel.hytale.server.core.HytaleServer;
-import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockBreakingDropType;
-import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockGathering;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.BreakBlockEvent;
-import com.hypixel.hytale.server.core.event.events.player.PlayerConnectEvent;
-import com.hypixel.hytale.server.core.inventory.Inventory;
-import com.hypixel.hytale.server.core.inventory.ItemStack;
-import com.hypixel.hytale.server.core.modules.block.BlockModule;
 import com.hypixel.hytale.server.core.modules.entity.item.ItemComponent;
-import com.hypixel.hytale.server.core.modules.entity.item.PickupItemComponent;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
-import com.hypixel.hytale.server.core.universe.world.SetBlockSettings;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import nl.trifox.foxprison.modules.mines.config.AutoResetDefinition;
 import nl.trifox.foxprison.modules.mines.data.MineRuntimeState;
@@ -33,7 +20,6 @@ import static com.hypixel.hytale.server.core.modules.interaction.BlockHarvestUti
 
 
 public class MineBlockBreakEvent extends EntityEventSystem<EntityStore, BreakBlockEvent> {
-
 
     private final MineService mineService;
 
@@ -61,11 +47,11 @@ public class MineBlockBreakEvent extends EntityEventSystem<EntityStore, BreakBlo
 
             BlockType blockType = world.getBlockType(pos.x, pos.y, pos.z);
             if (blockType == null) return;
-            world.breakBlock(pos.getX(), pos.getY(), pos.getZ(), SetBlockSettings.NO_DROP_ITEMS);
 
             var inv = player.getInventory();
             if (inv != null) {
-                var drops = getDrops(blockType, 1, breakBlockEvent.getBlockType().getId(), null);
+                var drops = getDrops(blockType, 1, null, blockType.getGathering().getBreaking().getDropListId());
+
                 if (inv.getCombinedBackpackStorageHotbar().canAddItemStacks(drops)) {
                     inv.getCombinedBackpackStorageHotbar().addItemStacks(drops);
                 }

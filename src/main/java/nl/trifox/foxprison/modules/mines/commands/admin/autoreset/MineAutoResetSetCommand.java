ce0950e7cd8bb655644f45a806a5140206e140e9
Calvin-Davidson <53999981+Calvin-Davidson@users.noreply.github.com>
2026-02-11 20:48:43 +0100

    fixed mine autoreset command and fixed response messages

--- src/main/java/nl/trifox/foxprison/modules/mines/commands/admin/autoreset/MineAutoResetSetCommand.java
+++ src/main/java/nl/trifox/foxprison/modules/mines/commands/admin/autoreset/MineAutoResetSetCommand.java
@@ -1,5 +1,6 @@
 package nl.trifox.foxprison.modules.mines.commands.admin.autoreset;
 
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.AbstractCommand;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.arguments.system.OptionalArg;
@@ -36,13 +37,23 @@ public final class MineAutoResetSetCommand extends AbstractCommand {
     @Override
     protected CompletableFuture<Void> execute(@NonNullDecl CommandContext context) {
         String id = mineId.get(context);
+        var optionalMine = service.getMine(id);
+        if (optionalMine.isEmpty()) {
+            context.sender().sendMessage(Message.translation("foxPrison.mines.not_exist"));
+            return CompletableFuture.completedFuture(null);
+        }
+
+        var existingMineResetDelay = optionalMine.get().getAutoReset().getMinSecondsBetweenResets();
 
         return service.setAutoReset(
                 id,
                 enabled.get(context),
                 intervalSeconds.get(context),
                 blocksBrokenThreshold.get(context),
-                minSecondsBetweenResets.provided(context) ? minSecondsBetweenResets.get(context) : null
-        ).thenApply(_ -> null);
+                minSecondsBetweenResets.provided(context) ? minSecondsBetweenResets.get(context) : existingMineResetDelay
+        ).thenApply(_ -> {
+            context.sender().sendMessage(Message.translation("foxPrison.mines.commands.autoreset.set.success"));
+            return null;
+        });
     }
 }
